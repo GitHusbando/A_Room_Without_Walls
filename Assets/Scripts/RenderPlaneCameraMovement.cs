253053fc7e3ce8c17b202ec7046fdcf91d3955bf
GitHusbando <67436617+GitHusbando@users.noreply.github.com>
2021-11-04 20:25:35 -0700

    mostly fixed rendertexture portals, still some height issues though

--- Assets/Scripts/RenderPlaneCameraMovement.cs
+++ Assets/Scripts/RenderPlaneCameraMovement.cs
@@ -6,18 +6,21 @@ public class RenderPlaneCameraMovement : MonoBehaviour
 {
     public Camera cameraToCopy;
 
+    public GameObject renderPlaneTarget;
+    public GameObject renderPlaneDestination;
+
     private Vector3 cameraOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        cameraOffset = transform.position - cameraToCopy.transform.position;
+        cameraOffset = renderPlaneTarget.transform.position - renderPlaneDestination.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = cameraToCopy.transform.position + cameraOffset;
+        transform.position = cameraToCopy.transform.position - cameraOffset;
         transform.rotation = cameraToCopy.transform.rotation;
     }
 }
