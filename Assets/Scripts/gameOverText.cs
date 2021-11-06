659eb6d5ee93a77d34ac4ef539eb1d0ee6a21278
Mateen Aminian <maaminia@ucsc.edu>
2021-11-06 15:29:59 -0700

    Fixed score count at gameover

--- Assets/Scripts/gameOverText.cs
+++ Assets/Scripts/gameOverText.cs
@@ -18,6 +18,6 @@ public class gameOverText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameoverText.text = "Treasures collected: " + treasureBehavior.score;
+        gameoverText.text = "Treasures collected: " + scoreCount.score;
     }
 }
