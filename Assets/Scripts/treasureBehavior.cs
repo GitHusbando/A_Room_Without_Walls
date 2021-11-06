d23ee363f62db1959dc2b5b4719e937586f771c9
Mateen Aminian <maaminia@ucsc.edu>
2021-11-06 15:08:19 -0700

    added pick up sound & instructions

--- Assets/Scripts/treasureBehavior.cs
+++ Assets/Scripts/treasureBehavior.cs
@@ -6,28 +6,37 @@ using UnityEngine;
 public class treasureBehavior : MonoBehaviour
 {
     
-
-
+    public static int score = 0;
+    
+    AudioSource source;
     public GameObject[] spawnLocations;
     public GameObject burger;
     public GameObject pumpkin;
     public GameObject tree;
     void Awake(){
         spawnLocations = GameObject.FindGameObjectsWithTag("SpawnPoint");
-
+        
     }
     void Start(){
         burger = (GameObject)Resources.Load("burgerPrefab", typeof(GameObject));
         pumpkin = (GameObject)Resources.Load("pumpkinPrefab", typeof(GameObject));
         tree = (GameObject)Resources.Load("OptimizedTree", typeof(GameObject));
-       
-
+        source = GetComponent<AudioSource>();
+        if (score > 0){
+            // play pick up sound 
+            source.Play(0);
+        }
+        
     }
     
-    void OnTriggerEnter(Collider col){
+ 
 
+    void OnTriggerEnter(Collider col){
+        //source.Play();
          if(col.tag =="Player"){
+            source.Play();
             Destroy(gameObject);
+            score++;
             GameObject temp = burger;
             int spawn = Random.Range(0,spawnLocations.Length);
             print("This is the length of the array " + spawnLocations.Length);
